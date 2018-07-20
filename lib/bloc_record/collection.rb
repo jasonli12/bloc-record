c7e15b8a06328f431c7262b62fa0086909aa14f5
Jason Li <jason.li0102@gmail.com>
2018-07-19 23:32:20 -0700

    Implement take, where and not instance methods for Collection class

--- lib/bloc_record/collection.rb
+++ lib/bloc_record/collection.rb
@@ -1,8 +1,32 @@
 module BlocRecord
   class Collection < Array
-    def update_all(array)
+    def update_all(updates)
       ids = self.map(&:id)
       self.any? ? self.first.class.update(ids, updates) : false
+    end
+
+    def take(limit = 1)
+      self.length > limit ? self.slice(0, limit) : self
+    end
+
+    def where(criteria)
+      subcollection = []
+      criteria_key = criteria.keys[0]
+      criteria_value = criteria.values[0]
+      self.each do |record|
+        subcollection.push(record) if record[criteria_key] == criteria_value
+      end
+      subcollection
+    end
+
+    def not(criteria)
+      subcollection = []
+      criteria_key = criteria.keys[0]
+      criteria_value = criteria.values[0]
+      self.each do |record|
+        subcollection.push(record) if record[criteria_key] != criteria_value
+      end
+      subcollection
     end
   end
 end
